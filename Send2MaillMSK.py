830062168c01b893eba3403ccd2cc845af95ac3e
ACTSP <108674672+actsp@users.noreply.github.com>
2024-01-04 10:06:12 -0300

    Update Send2MaillMSK.py
    Adicionado a Função IsNiver(dataNasc) para verificar se a data de nascimento faz aniversário na data de hoje.

--- Send2MaillMSK.py
+++ Send2MaillMSK.py
@@ -1,12 +1,14 @@
 import smtplib
+import pandas as pd
+import pytz
+import datetime
+from datetime import datetime
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
-import pytz
-from datetime import datetime
 
 def Send2Mail(FileName,sender_email, recipient_email, subject, body):
     #https://github.com/aws/aws-sdk-js/issues/893
@@ -29,3 +31,14 @@ def Send2Mail(FileName,sender_email, recipient_email, subject, body):
         datetime_br= datetime.now(pytz.timezone('America/Sao_Paulo'))                
         msgRESP = 'Enviado para: ' + str(recipient_email) + ' em: ' + str(datetime_br.strftime('%d/%m/%Y %H:%M:%S'))
         return msgRESP
+
+def IsNiver(data):
+  from datetime import datetime
+  # storing the current time in the variable
+  c = datetime.now()
+  df = pd.to_datetime(data, format='%m/%d/%Y')
+  d = pd.to_datetime(c,format='%Y-%m-%d')
+  if df.month == d.month and df.day == d.day:
+    return True
+  else:
+    return False
